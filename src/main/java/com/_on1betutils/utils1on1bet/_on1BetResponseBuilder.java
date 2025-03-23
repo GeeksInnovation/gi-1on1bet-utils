094bb29351153b6f6ef379feb6567a5aedd1653b
siva <siva.velusamy.kgm@gmail.com>
2025-03-23 22:22:54 +0530

    typo issue fixed

--- src/main/java/com/_on1betutils/utils1on1bet/_on1BetResponseBuilder.java
+++ src/main/java/com/_on1betutils/utils1on1bet/_on1BetResponseBuilder.java
@@ -1,6 +1,6 @@
 package com._on1betutils.utils1on1bet;
 
-public class _on1BetResponeBuilder {
+public class _on1BetResponseBuilder {
 
     public <T> _on1BetResponse<T> buildSuccessResponse(T data) {
         return new _on1BetResponse<>(true, null, data);
